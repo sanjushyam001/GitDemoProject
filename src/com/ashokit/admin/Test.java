13dd659743d1c9b1eebb15bf9cb1f76e5aa5f06d
sanjeev <kumarsanjuashokit@gmail.com>
2023-03-21 16:02:50 +0530

    commiting changes to BranchX

--- src/com/ashokit/admin/Test.java
+++ src/com/ashokit/admin/Test.java
@@ -4,5 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Git Practice has started now onwards so be attentive");
+		
+		Student student=new Student();
+		student.setName("Sanjeev Kumar");
+		System.out.println("Student Name: "+student.getName());
 	}
 }
